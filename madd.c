4a4f2d4448fd82f1b92afc5817ce16fdb449290c
ksm22016 <kyle.minott@uconn.edu>
2025-11-04 03:17:29 +0000

    fixed

--- madd.c
+++ madd.c
@@ -16,28 +16,27 @@ static void * thread_main(void * p_arg)
     // TODO
     thread_arg_t *arg = (thread_arg_t *)p_arg;
 
-    TMatrix *m = arg->m;
-    TMatrix *n = arg->n;
-    TMatrix *t = arg->t;
-
-    unsigned int rows = m->nrows;
-    unsigned int cols = m->ncols;
+    unsigned int rows = arg->m->nrows;
+    unsigned int cols = arg->m->ncols;
     unsigned int id   = arg->id;
 
-    unsigned int base = rows / NUM_THREADS;
-    unsigned int rem  = rows % NUM_THREADS;
+    /* split rows as evenly as possible */
+    unsigned int base      = rows / NUM_THREADS;
+    unsigned int rem       = rows % NUM_THREADS;
+    unsigned int start_row = id * base + (id < rem ? id : rem);
+    unsigned int row_count = base + (id < rem ? 1u : 0u);
 
-    unsigned int start = id * base + (id < rem ? id : rem);
-    unsigned int count = base + (id < rem ? 1u : 0u);
-    unsigned int end   = start + count;
+    /* process a single contiguous block for better cache/SIMD */
+    TElement *tptr = &arg->t->data[start_row][0];
+    TElement *mptr = &arg->m->data[start_row][0];
+    TElement *nptr = &arg->n->data[start_row][0];
 
-    for (unsigned int i = start; i < end; ++i) {
-        for (unsigned int j = 0; j < cols; ++j) {
-            t->data[i][j] = m->data[i][j] + n->data[i][j];
+    unsigned int count = row_count * cols;
+    for (unsigned int k = 0; k < count; ++k) {
+        tptr[k] = mptr[k] + nptr[k];
         }
+        return NULL;
     }
-    return NULL;
-}
 
 /* Return the sum of two matrices. The result is in a newly creaed matrix. 
  *
@@ -62,12 +61,13 @@ TMatrix * addMatrix_thread(TMatrix *m, TMatrix *n)
 
     for (unsigned int i = 0; i < NUM_THREADS; ++i) {
         args[i].id = i;
-        args[i].m = m;
-        args[i].n = n;
-        args[i].t = t;
+        args[i].m  = m;
+        args[i].n  = n;
+        args[i].t  = t;
         int rc = pthread_create(&threads[i], NULL, thread_main, &args[i]);
         if (rc != 0) {
-            perror("pthread_create");
+            /* Spec: if a pthread function fails, report error and exit */
+            fprintf(stderr, "pthread_create failed (rc=%d)\n", rc);
             freeMatrix(t);
             exit(EXIT_FAILURE);
         }
@@ -76,7 +76,7 @@ TMatrix * addMatrix_thread(TMatrix *m, TMatrix *n)
     for (unsigned int i = 0; i < NUM_THREADS; ++i) {
         int rc = pthread_join(threads[i], NULL);
         if (rc != 0) {
-            perror("pthread_join");
+            fprintf(stderr, "pthread_join failed (rc=%d)\n", rc);
             freeMatrix(t);
             exit(EXIT_FAILURE);
         }
